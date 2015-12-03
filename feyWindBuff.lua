b09df708534ce1fb11a7b2c787b999ba2ef46c05
Lucas AZAÏS <lucasazais@gmail.com>
2015-12-03 21:20:23 +0000

    Added feyWind icon

---
+++ feyWindBuff.lua
@@ -0,0 +1,12 @@
+
+-- Table containing the Fey Wind image
+feyWindBuff = {}
+feyWindBuff.level = 0
+feyWindBuff.filepath = "fey_wind.png"
+feyWindBuff.tint = "#FFFFFFFF"
+feyWindBuff.x = math.floor(UI.PrimaryMonitorWidth / 2) + 300
+feyWindBuff.y = math.floor(UI.PrimaryMonitorHeight / 2) - 1900
+feyWindBuff.scalex = 1
+feyWindBuff.scaley = 1
+feyWindBuff.rotation = 0
+feyWindBuff.hidden = true
